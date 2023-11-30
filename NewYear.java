48528333042b0f4cd18c43b775f413d0ad8c0512
daisuki0i <daisuki0i@daisuki0is-MacBook-Pro.local>
2023-12-01 01:10:24 +0700

    first commit

---
+++ NewYear.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class NewYear extends JPanel {
+    final Color MIDNIGHT_BLUE = new Color(54, 69, 79);
+    final Color BACKGROUND = new Color(96, 130, 182);
+    final Color CAMEL = new Color(193, 154, 107);
+
+    public static void main(String[] args) {
+        NewYear n = new NewYear();
+
+        JFrame f = new JFrame();
+        f.add(n);
+        f.setTitle("NewYear");
+        f.setSize(700, 700);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.setLocationRelativeTo(null); // หน้าต่างอยู่ตรงกลาง
+    }
+
+    public void paintComponent(Graphics g) {
+        g.setColor(BACKGROUND);
+        drawRectangle(g, 0, 0, 700, 700);
+
+        g.setColor(CAMEL);
+        drawRectangle(g, 0, 500, 700, 700);
+
+        g.setColor(MIDNIGHT_BLUE);
+        drawRectangle(g, 500, 300, 700, 500);
+    }
+
+    private void plot(Graphics g, int x, int y) {
+        g.fillRect(x, y, 1, 1);
+    }
+
+    private void drawRectangle(Graphics g, int x1, int y1, int x2, int y2) {
+        int x = x1;
+        int y = y1;
+
+        for (int i = x; i < x2; i++) {
+            for (int j = y; j < y2; j++) {
+                plot(g, i, j);
+            }
+        }
+    }
+}
