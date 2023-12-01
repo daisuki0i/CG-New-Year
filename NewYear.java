ffca0fcc5fb8d340ef694658af308a1820c38cd8
ModzabazeR <moddaeng.pps@gmail.com>
2023-12-01 22:54:17 +0700

    edit method

--- NewYear.java
+++ NewYear.java
@@ -19,25 +19,21 @@ public class NewYear extends JPanel {
     }
 
     public void paintComponent(Graphics g) {
-        g.setColor(BACKGROUND);
-        drawRectangle(g, 0, 0, 600, 600);
+        drawRectangle(g, 0, 0, 600, 600, BACKGROUND);
 
-        g.setColor(CAMEL);
-        drawRectangle(g, 0, 425, 600, 600);
+        drawRectangle(g, 0, 425, 600, 600, CAMEL);
 
-        g.setColor(Color.WHITE);;
-        drawRectangle(g, 400, 50, 600, 250);
-        drawRectangle(g, 390, 250, 600, 280);
-        drawRectangle(g, 410, 280, 600, 290);
-
-        
+        drawRectangle(g, 400, 50, 600, 250, Color.WHITE);
+        drawRectangle(g, 390, 250, 600, 280, Color.WHITE);
+        drawRectangle(g, 410, 280, 600, 290, Color.WHITE);
     }
 
     private void plot(Graphics g, int x, int y) {
         g.fillRect(x, y, 1, 1);
     }
 
-    private void drawRectangle(Graphics g, int x1, int y1, int x2, int y2) {
+    private void drawRectangle(Graphics g, int x1, int y1, int x2, int y2, Color color) {
+        g.setColor(color);
         for (int x = x1; x < x2; x++) {
             for (int y = y1; y < y2; y++) {
                 plot(g, x, y);
