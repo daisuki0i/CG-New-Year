741c9a865cc70c4d4eef75f1f4fffded0a5bda21
ModzabazeR <moddaeng.pps@gmail.com>
2023-12-02 00:50:15 +0700

    add custom color class

--- NewYear.java
+++ NewYear.java
@@ -1,33 +1,40 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class NewYear extends JPanel {
-    final Color WINDOW = new Color(236, 186, 88);
-    final Color BACKGROUND = new Color(235, 223, 171);
-    final Color TABLE = new Color(87, 66, 65);
-    final Color NIGHTTIME = new Color(19, 84, 122);
+class MyColor extends Color {
+    public MyColor(int r, int g, int b) {
+        super(r, g, b);
+    }
 
+    public static final Color WINDOW = new Color(236, 186, 88);
+    public static final Color BACKGROUND = new Color(235, 223, 171);
+    public static final Color TABLE = new Color(87, 66, 65);
+    public static final Color NIGHTTIME = new Color(19, 84, 122);
+    public static final Color LINE = BLACK;
+}
+
+public class NewYear extends JPanel {
     public static void main(String[] args) {
         NewYear n = new NewYear();
 
         JFrame f = new JFrame();
         f.add(n);
         f.setTitle("NewYear");
-        f.setSize(600,600);
+        f.setSize(600, 600);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
         f.setLocationRelativeTo(null); // หน้าต่างอยู่ตรงกลาง
     }
 
     public void paintComponent(Graphics g) {
-        drawRectangle(g, 0, 0, 600, 600, BACKGROUND);
+        drawRectangle(g, 0, 0, 600, 600, MyColor.BACKGROUND);
 
-        drawRectangle(g, 0, 425, 600, 600, TABLE); // โต๊ะ
-        drawRectangle(g, 0, 423, 600, 425, Color.BLACK); // ขอบโต๊ะ
+        drawRectangle(g, 0, 425, 600, 600, MyColor.TABLE); // โต๊ะ
+        drawRectangle(g, 0, 423, 600, 425, MyColor.BLACK); // ขอบโต๊ะ
 
-        drawRectangle(g, 400, 50, 600, 250, WINDOW); // หน้าต่าง
-        drawRectangle(g, 390, 250, 600, 280, WINDOW);
-        drawRectangle(g, 410, 280, 600, 290, WINDOW);
+        drawRectangle(g, 400, 50, 600, 250, MyColor.WINDOW); // หน้าต่าง
+        drawRectangle(g, 390, 250, 600, 280, MyColor.WINDOW);
+        drawRectangle(g, 410, 280, 600, 290, MyColor.WINDOW);
 
         drawHorizontalLine(g, 400, 49, 200); // ขอบหน้าต่างแนวนอน
         drawHorizontalLine(g, 390, 250, 210);
@@ -38,12 +45,12 @@ public class NewYear extends JPanel {
         drawVerticalLine(g, 390, 250, 31);
         drawVerticalLine(g, 410, 280, 11);
 
-        drawRectangle(g, 420, 70, 600, 235, NIGHTTIME);
+        drawRectangle(g, 420, 70, 600, 235, MyColor.NIGHTTIME);
 
         drawHorizontalLine(g, 420, 70, 180);
         drawHorizontalLine(g, 420, 235, 180);
 
-         drawVerticalLine(g, 419, 70, 167);
+        drawVerticalLine(g, 419, 70, 167);
 
     }
 
@@ -60,11 +67,11 @@ public class NewYear extends JPanel {
         }
     }
 
-    private void drawHorizontalLine(Graphics g, int x1, int y1, int len){
-        drawRectangle(g, x1, y1, x1+len, y1+2, Color.BLACK);
+    private void drawHorizontalLine(Graphics g, int x1, int y1, int len) {
+        drawRectangle(g, x1, y1, x1 + len, y1 + 2, MyColor.LINE);
     }
 
-     private void drawVerticalLine(Graphics g, int x1, int y1, int len){
-        drawRectangle(g, x1, y1, x1+2, y1+len, Color.BLACK);
+    private void drawVerticalLine(Graphics g, int x1, int y1, int len) {
+        drawRectangle(g, x1, y1, x1 + 2, y1 + len, MyColor.LINE);
     }
 }
